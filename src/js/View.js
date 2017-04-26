3c923ecfe17344356a93c3ef684ed6cb54fcab29
Benjamin Pylko <brpylko@gmail.com>
2017-04-26 18:29:22 -0400

    Added render function

--- src/js/View.js
+++ src/js/View.js
@@ -1,4 +1,10 @@
 class View {
+    constructor(tag = "span") {
+        this.el = document.createElement(tag)
+    }
+    render(targetElement) {
+        targetElement.appendChild(this.el)
+    }
 }
 
 module.exports = View
